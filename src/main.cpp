207c7ab509c61bdab8064d296bfefe229834e11e
diljale <bhavendedhia@gmail.com>
2017-09-08 16:54:06 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,8 @@ double rad2deg(double x) { return x * 180 / pi(); }
 
 //global vaiables
 double g_throttle = 0.3;
+bool g_no_throttle = false;
+
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
@@ -36,7 +38,7 @@ int main()
   uWS::Hub h;
 
   PID pid;
-  pid.Init(.10, 0.001, 4.0);
+  pid.Init(.10, 0.0001, 4.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -71,8 +73,11 @@ int main()
           else
             g_throttle = 0;
            
-          if(cte > 0.2)
+          if(cte > 0.2 && !g_no_throttle)
+          {
             g_throttle = 0;
+            g_no_throttle = !g_no_throttle;
+          }
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << "Speed: "<< speed << std::endl;
