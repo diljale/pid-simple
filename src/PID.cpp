a254e4dafe477960c3146615eb3f83821baf03b9
diljale <bhavendedhia@gmail.com>
2017-09-08 14:54:10 -0700

    Update PID.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,23 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  Kp_ = Kp;
+  Ki_ = Ki;
+  Kd_ = Kd;
+  prev_cte_ = 0;
+  cte_total_ = 0;
 }
 
 void PID::UpdateError(double cte) {
+  cte_total_ += cte;
+  p_error = Kp_ * cte;
+  d_error = Kd_ * (cte - prev_cte_);
+  i_error = Ki_ * cte_total_;
+  
+  prev_cte_ = cte;
 }
 
 double PID::TotalError() {
+  return p_error+d_error+i_error;
 }
 
