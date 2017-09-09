d900df8fdcd5f87311879717aa1fde9ce487c629
diljale <bhavendedhia@gmail.com>
2017-09-08 17:06:53 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ int main()
   // Tried using different values of "p" component and see thatcar oscillates a lot if it is too high and doesn't really recover if its too low
   // Tried playing with "i" component, this is good to stabilize vehicle and increase speed
   // tried playing with "d" component, this helps reduce oscillations and compensates for "p" component
-  pid.Init(.10, 0.001, 4.5);
+  pid.Init(.10, 0.0001, 3.5);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -76,7 +76,7 @@ int main()
           else
             g_throttle = 0.1;
            
-          if(cte > 0.2)
+          if(abs(cte) > 0.2)
           {
             g_throttle = 0.1;
           }
